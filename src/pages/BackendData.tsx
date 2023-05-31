6280a2136b484c937f3cc95ec65d8d657b1e492d
Niko Helle <niko.hele@hyperactive.fi>
2023-05-31 15:15:14 +0300

    (HP-1778) ApiAccessTokenProvider came obsolete and was removed

--- src/pages/BackendData.tsx
+++ src/pages/BackendData.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import PageContent from '../components/PageContent';
 import LoginInfo from '../components/LoginInfo';
 import AuthenticatingInfo from '../components/AuthenticatingInfo';
-import { ApiAccessTokenProvider } from '../components/ApiAccessTokenProvider';
 import WithAuth from '../client/WithAuth';
 import BackendDataEditor from '../components/BackendDataEditor';
 
 const BackendData = (): React.ReactElement => (
   <PageContent>
-    <ApiAccessTokenProvider>
-      {WithAuth(BackendDataEditor, LoginInfo, AuthenticatingInfo)}
-    </ApiAccessTokenProvider>
+    {WithAuth(BackendDataEditor, LoginInfo, AuthenticatingInfo)}
   </PageContent>
 );
 
