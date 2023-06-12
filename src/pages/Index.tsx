85b466aca98273e2e1cdf189c971f921c6821efa
Niko Helle <niko.hele@hyperactive.fi>
2023-06-12 23:43:54 +0300

    (fix-deployments) (HP-1778) Fixed text typo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -26,7 +26,7 @@ const Index = (): React.ReactElement => {
             Voit kirjautua sisään / ulos alla olevasta komponentista tai
             headerista.
           </p>
-          <p>Voit myös kirjatua ulos toisessa ikkunassa.</p>
+          <p>Voit myös kirjautua ulos toisessa ikkunassa.</p>
           <LoginComponent />
           <ReduxConsumer />
           <WithAuthDemo />
